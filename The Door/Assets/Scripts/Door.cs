d25129286812e6d6c4a35b14867e2d9b809b16c7
breno morais <brenomorais88@gmail.com>
2016-07-09 13:12:01 -0300

    }first commit

---
+++ The Door/Assets/Scripts/Door.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	public GameObject [] exitDoors;
+	public GameObject [] cameras;
+	public Texture [] texturasPortas;
+	public GameObject player;
+	public enum tipoPorta {Principal, Secundaria};
+	public tipoPorta tipoDaPorta;
+	private int portaAtiva = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (cameras.Length != 0)
+		{
+			cameras[portaAtiva].transform.rotation = player.transform.rotation;
+		}
+
+		switch (tipoDaPorta)
+		{
+			case tipoPorta.Principal:
+				if (Input.GetKeyDown(KeyCode.F))
+				{
+					portaAtiva = (int)Mathf.Repeat(portaAtiva + 1, exitDoors.Length);
+
+					GetComponent<MeshRenderer>().material.mainTexture = texturasPortas[portaAtiva];
+				}
+			break;
+			case tipoPorta.Secundaria:
+			break;
+		}
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			//other.transform.position = new Vector3(exitDoors[portaAtiva].transform.position.x, exitDoors[portaAtiva].transform.position.y, exitDoors[portaAtiva].transform.position.z + exitDoors[portaAtiva].transform.forward.z * other.transform.localScale.z);
+			other.transform.position = exitDoors[portaAtiva].transform.position + exitDoors[portaAtiva].transform.up * other.transform.localScale.z;
+		}
+	}
+}
---
+++ The Door/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float velocidade = 5;
+	public float velocidadeRotacao = 5;
+	private Rigidbody rb;
+	private float velocidadeAntiga;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody>();
+		velocidadeAntiga = velocidade;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//transform.Translate(Vector3.forward * Input.GetAxisRaw("Vertical") * Time.deltaTime * velocidade);
+
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			velocidadeAntiga = velocidade;
+			velocidade *= 2;
+		}
+		else if (Input.GetKeyUp(KeyCode.LeftShift))
+		{
+			velocidade = velocidadeAntiga;
+		}
+
+		transform.RotateAround(transform.position, Vector3.up, Input.GetAxisRaw("Horizontal") * velocidadeRotacao);
+	}
+
+	void FixedUpdate()
+	{
+		rb.MovePosition(transform.position + transform.forward * velocidade * Input.GetAxisRaw("Vertical"));
+	}
+}
